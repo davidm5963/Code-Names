7efb436913e955b2ece3451e831cf167fe1225d9
David Moeller <Davidmoeller115963@Gmail.com>
2018-07-14 00:14:23 -0400

    import components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { BoardComponent } from './board/board.component';
+import { CardComponent } from './card/card.component';
+import { FindGameComponent } from './find-game/find-game.component';
+import { CreateGameComponent } from './create-game/create-game.component';
+import { GameWaitingComponent } from './game-waiting/game-waiting.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BoardComponent,
+    CardComponent,
+    FindGameComponent,
+    CreateGameComponent,
+    GameWaitingComponent,
   ],
   imports: [
     BrowserModule
