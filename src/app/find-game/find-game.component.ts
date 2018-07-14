458a5716ce852b932e9f8072ccce39a6dfb092f7
David Moeller <Davidmoeller115963@Gmail.com>
2018-07-14 00:41:22 -0400

    set up form

--- src/app/find-game/find-game.component.ts
+++ src/app/find-game/find-game.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { GameService } from '../services/game.service';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-find-game',
@@ -7,9 +9,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FindGameComponent implements OnInit {
 
-  constructor() { }
+  joinGameForm: FormGroup;
+
+  constructor(private gameService: GameService, private fb: FormBuilder) { }
 
   ngOnInit() {
+    this.joinGameForm = this.fb.group({
+      'gameId' : ['', [Validators.required]],
+      'userName' : ['', [Validators.required]]
+    });
+  }
+
+  joinGame(){
+    this.gameService.joinGame(this.gameId, this.userName);
+  }
+
+  get gameId(){
+    return this.joinGameForm.get('gameId');
+  }
+
+  get userName(){
+    return this.joinGameForm.get('userName');
   }
 
 }
