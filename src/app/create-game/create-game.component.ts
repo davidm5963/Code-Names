e79e0883cd8c323b35c8b7d04dc3f35368f91bc9
David Moeller <Davidmoeller115963@Gmail.com>
2018-07-14 08:14:37 -0400

    add createGame()

--- src/app/create-game/create-game.component.ts
+++ src/app/create-game/create-game.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { GameService } from '../services/game.service';
 
 @Component({
   selector: 'app-create-game',
@@ -7,9 +8,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CreateGameComponent implements OnInit {
 
-  constructor() { }
+  constructor(private gameService: GameService) { }
 
   ngOnInit() {
+  }
+
+  createGame(){
+    this.gameService.createGame('David');
   }
 
 }
