ad03794555e3d5f01ba0f0ad47fd55cf109daa0b
David Moeller <Davidmoeller115963@Gmail.com>
2018-07-14 08:58:31 -0400

    import broswer animations

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { FormsModule, ReactiveFormsModule, FormBuilder } from '@angular/forms';
 import { RouterModule } from '@angular/router'
@@ -7,12 +8,21 @@ import { RouterModule } from '@angular/router'
 import { AngularFireModule } from 'angularfire2';
 import { AngularFirestoreModule } from 'angularfire2/firestore'
 
+//angular material modules
+import {MatInputModule} from '@angular/material';
+import {MatButtonModule} from '@angular/material';
+import {MatFormFieldModule} from '@angular/material';
+import {MatCheckboxModule} from '@angular/material';
+
+
+
 import { AppComponent } from './app.component';
 import { BoardComponent } from './board/board.component';
 import { CardComponent } from './card/card.component';
 import { FindGameComponent } from './find-game/find-game.component';
 import { CreateGameComponent } from './create-game/create-game.component';
 import { GameWaitingComponent } from './game-waiting/game-waiting.component';
+
 
 import { GameService } from './services/game.service';
 
@@ -28,7 +38,11 @@ import { environment } from '../environments/environment';
     GameWaitingComponent,
   ],
   imports: [
+    MatButtonModule,
+    MatInputModule,
+
     BrowserModule,
+    BrowserAnimationsModule,
     AngularFirestoreModule,
     ReactiveFormsModule,
     FormsModule,
