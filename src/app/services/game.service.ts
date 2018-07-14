9a638ef9431fa652dc0c1f9a724b33afec709211
David Moeller <Davidmoeller115963@Gmail.com>
2018-07-14 08:09:34 -0400

    add joinGame(), createGame()

--- src/app/services/game.service.ts
+++ src/app/services/game.service.ts
@@ -1,9 +1,43 @@
 import { Injectable } from '@angular/core';
+import { AngularFirestore } from 'angularfire2/firestore';
 
 @Injectable({
   providedIn: 'root'
 })
 export class GameService {
 
-  constructor() { }
+  constructor(private afs: AngularFirestore) { }
+
+  joinGame(userName: string, gameId: string){
+    this.getGameById(gameId).get().then(doc => {
+      console.log(doc.data());
+        let players = doc.get('players');
+        players.push({
+          userName: userName,
+          role: '',
+          isCreator: false
+        })
+        this.getGameById(gameId).update({players: players})
+      }
+    )
+  }
+
+  createGame(userName: string){
+    this.afs.collection('games').add(
+      {
+        status: 'waiting',
+        board: [],
+        players: [{
+          userName: userName,
+          role: '',
+          isCreator: true
+        }]
+
+      }
+    )
+  }
+
+  getGameById(gameId: string){
+    return this.afs.doc('games/'+gameId).ref;
+  }
 }
