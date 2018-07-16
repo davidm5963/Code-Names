8208d59f5959977eab111c1f2c26711aa16f4759
David Moeller <Davidmoeller115963@Gmail.com>
2018-07-16 10:08:27 -0400

    support for signing in players anonymously, getting players

--- src/app/services/game.service.ts
+++ src/app/services/game.service.ts
@@ -1,8 +1,13 @@
 import { Injectable } from '@angular/core';
-import { AngularFirestore, AngularFirestoreDocument } from 'angularfire2/firestore';
+import { AngularFirestore, AngularFirestoreDocument, AngularFirestoreCollection } from 'angularfire2/firestore';
 import { Game } from '../models/game';
 import { Player } from '../models/player';
 import { Router } from '@angular/router';
+import { Observable } from 'rxjs';
+
+import { AngularFireAuth } from 'angularfire2/auth';
+
+import * as firebase from 'firebase/app';
 
 @Injectable({
   providedIn: 'root'
@@ -11,56 +16,96 @@ export class GameService {
 
   currentGame: AngularFirestoreDocument<Game>;
   currentPlayerId: string;
+  
+  currentPlayer: AngularFirestoreDocument<any>;
+  players: AngularFirestoreCollection<Player>;
 
-  constructor(private afs: AngularFirestore, private router: Router) { }
+  constructor(private afs: AngularFirestore, private router: Router, private afAuth: AngularFireAuth) {
+   }
+
+  signIn(){
+    firebase.auth().setPersistence(firebase.auth.Auth.Persistence.LOCAL)
+      .then(function() {
+          firebase.auth().signInAnonymously().then(user =>{
+              this.currentPlayerId = user.user.uid;
+            }
+            ).catch(function(error) {
+            // Handle Errors here.
+            console.log(error.message);
+            // ...
+          });
+      });
+  }
 
   joinGame(gameId: string, userName: string){
 
-    this.afs.collection('players').add({
-      userName: userName,
-      isCreator: false,
-      role: '',
-      team: ''
-    }).then(user => {
-      this.currentPlayerId = user.id;
-      this.getGameById(gameId).ref.get().then(game => {
-        let players = game.get('players');
-        players.push({
-          playerId: this.currentPlayerId
-        });
-        this.router.navigate(['game/waiting', game.id])
-        
-        this.getGameById(gameId).update({players: players});
+    this.signIn();
+
+    this.setPlayerDoc(userName, false, gameId).then(user => {
+
+        this.getGameById(gameId).ref.get().then(game => {
+          this.router.navigate(['game/waiting', gameId]);
         }
       )
     });    
   }
 
+  private setPlayerDoc(userName: string, isCreator: boolean, gameId: string){
+      const playerRef: AngularFirestoreDocument<Player> = this.afs.doc(`games/${gameId}/players/${firebase.auth().currentUser.uid}`);
+  
+      const data: Player = {
+        userName: userName,
+        isCreator: isCreator,
+        role: '',
+        team: '',
+        gameId: gameId
+      }
+      this.currentPlayer = playerRef;
+
+      return playerRef.set(data)
+  }
+
   createGame(userName: string){
-    this.afs.collection('players').add({
-      userName: userName,
-      isCreator: true,
-      role: '',
-      team: ''
-    }).then(creator => {
-      this.afs.collection('games').add(
+
+    this.signIn();
+    
+    this.afs.collection('games').add(
         {
           status: 'waiting',
           board: [],
-          players: [{
-            playerId: creator.id
-          }]
         }
       ).then(game => {
+          this.setPlayerDoc(userName, true, game.id);
+          
           this.router.navigate(['game/waiting/', game.id])        
         }
-      );
-    })
-    
+      );    
   }
 
   getGameById(gameId: string){
     this.currentGame = this.afs.doc('games/'+gameId);
     return this.currentGame;
   }
+
+  getPlayerById(playerId: string){
+    return this.afs.doc<Player>('players/'+playerId);
+  }
+
+  getPlayers(gameId: string){
+    this.players = this.afs.doc(`games/${gameId}`).collection('players');
+    return this.players;
+  }
+
+  getCurrentPlayer(gameId: string){
+    return this.afs.doc(`games/${gameId}/players/${firebase.auth().currentUser.uid}`);
+  }
+
+  startGame(gameId: string){
+    this.afs.doc(`games/${gameId}`).update({status: 'in progress'});
+    this.createBoard(gameId);
+  }
+
+  
+
+  
 }
