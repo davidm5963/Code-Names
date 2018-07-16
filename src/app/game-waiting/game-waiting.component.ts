ee0a7c05edf4a54130650b82ba245ec993ebc274
David Moeller <Davidmoeller115963@Gmail.com>
2018-07-16 09:45:05 -0400

    get players for game

--- src/app/game-waiting/game-waiting.component.ts
+++ src/app/game-waiting/game-waiting.component.ts
@@ -1,4 +1,12 @@
 import { Component, OnInit } from '@angular/core';
+import { GameService } from '../services/game.service';
+import { Player } from '../models/player';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Game } from '../models/game';
+import { Observable } from 'rxjs';
+
+import * as firebase from 'firebase/app';
+import { SELECT_PANEL_INDENT_PADDING_X } from '@angular/material';
 
 @Component({
   selector: 'app-game-waiting',
@@ -7,9 +15,43 @@ import { Component, OnInit } from '@angular/core';
 })
 export class GameWaitingComponent implements OnInit {
 
-  constructor() { }
+  gameId: string;
+  game: Observable<Game>;
+  players: Observable<Player[]>;
+  currentPlayer: any;
+
+  constructor(private gameService: GameService, private route: ActivatedRoute, private router: Router) {
+
+   }
+   
+   sleep (time) {
+    return new Promise((resolve) => setTimeout(resolve, time));
+  }
 
   ngOnInit() {
+    console.log("ngOnInit called")
+    this.gameId = this.route.snapshot.params['gameId'];
+
+    this.gameService.getGameById(this.gameId).valueChanges().subscribe(game => {
+      if(game.status == 'in progress'){
+        this.router.navigate(['game/board/', this.gameId])
+      }
+    });
+    
+    this.players = this.gameService.getPlayers(this.gameId).valueChanges();
+
+    this.getUser();
+  }
+  getUser(){
+    this.sleep(1000).then(() =>{
+      this.gameService.getCurrentPlayer(this.gameId).valueChanges().subscribe(player =>{
+        console.log(player)
+        this.currentPlayer = player;
+      });  
+    })
+  }
+  onStartGame(){
+    this.gameService.startGame(this.gameId);
   }
 
 }
