f8939fa6297fffe038a1645db80de0947cd49eca
David Moeller <Davidmoeller115963@Gmail.com>
2018-07-17 13:07:33 -0400

    change route

--- src/app/Routes.ts
+++ src/app/Routes.ts
@@ -2,10 +2,11 @@ import { Routes } from '@angular/router';
 import { FindGameComponent } from './find-game/find-game.component';
 import { GameWaitingComponent } from './game-waiting/game-waiting.component';
 import { BoardComponent } from './board/board.component';
+import { GameComponent } from './game/game.component';
 
 export const appRoutes: Routes = [
     { path: 'findGame', component: FindGameComponent },
     { path: 'game/waiting/:gameId', component: GameWaitingComponent },
-    { path: 'game/board/:gameId', component: BoardComponent},
+    { path: 'game/board/:gameId', component: GameComponent},
     { path: '', redirectTo: '/findGame', pathMatch: 'full'},
 ];
