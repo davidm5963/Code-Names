6915a5ed31c2308e36a768bad43c824a9e695a05
David Moeller <Davidmoeller115963@Gmail.com>
2018-07-17 13:09:53 -0400

    add functionality for swithing turn and changing card color

--- src/app/board/board.component.ts
+++ src/app/board/board.component.ts
@@ -14,22 +14,41 @@ export class BoardComponent implements OnInit {
   gameId: string;
   cards: any;
   isCodeMaster: boolean;
+  currentTurn: string;
+  isTurn: boolean;
+  playerTeam: string;
 
   constructor(private gameService: GameService, private route: ActivatedRoute, private playerService: PlayerService) { }
 
   ngOnInit() {
     this.gameId = this.route.snapshot.params['gameId'];
+
     this.cards = this.gameService.getGameCards(this.gameId).valueChanges();
+
     this.playerService.getCurrentPlayer(this.gameId).subscribe(player => {
       this.isCodeMaster = player.role == 'codemaster';
+      this.playerTeam = player.team;
+      console.log("Player team: " + this.playerTeam);
+
+      this.gameService.getGameById(this.gameId).valueChanges().subscribe(game => {
+        console.log("Game turn: " + game.turn);
+        this.isTurn = game.turn === this.playerTeam;
+        console.log(this.isTurn)
+      }) 
     })
+
+ 
   }
 
   onCardClick(card: Card)
   {
-    if(!this.isCodeMaster){
-
+    if(!this.isCodeMaster && this.isTurn){
+      this.gameService.changeCardStatus(card, this.gameId);
     }
+  }
+
+  endTurn(){
+    this.gameService.switchTurns(this.gameId, this.playerTeam);
   }
 
 
