1c6435bb1c3a88cde0633bb0d4bb51280f3437b2
David Moeller <Davidmoeller115963@Gmail.com>
2018-07-17 13:10:09 -0400

    initial commit

---
+++ src/app/clue-list/clue-list.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-clue-list',
+  templateUrl: './clue-list.component.html',
+  styleUrls: ['./clue-list.component.css']
+})
+export class ClueListComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
